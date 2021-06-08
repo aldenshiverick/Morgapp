c402e27b88e85531c8d7a0989b4822769acbdbb7
Morgan <morgan.smith118@gmail.com>
2021-06-08 13:35:07 -0600

    Update p1Profile.js

--- assets/js/p1Profile.js
+++ assets/js/p1Profile.js
@@ -96,7 +96,7 @@ function updateUserValues(){
   console.log('User APIid: ' + user);
   let at = "Bearer " + Cookies.get("accessToken");
   let url = apiUrl + "/environments/" + environmentID + "/users/" + user;
-  let list = [username, $('#username').val()];
+  let list = ['username', $('#username').val()];
   let i = 1;
   //list[0].push('username:' + $('#username').val());
   console.log('current array: ' +JSON.stringify(list) );
