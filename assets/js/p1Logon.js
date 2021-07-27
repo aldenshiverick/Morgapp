abe69148fa4a13f7b9d44cdc846b8354616a61bc
Morgan <morgan.smith118@gmail.com>
2021-07-27 13:50:39 -0600

    Update p1Logon.js

--- assets/js/p1Logon.js
+++ assets/js/p1Logon.js
@@ -641,7 +641,7 @@ function checkpwned(){
 
 function parsepwned(data,password){
   let pwdstring= password.substring(5,password.length+1);
-  pwdstring=pwdString.toUpperCase();
+  pwdstring=pwdstring.toUpperCase();
   //let pwdArry = data.split("\n");
   //let pwdArry = data.split("/\r?\n/");
   let result = "SAFE";
