fd74548c40e13aa8a42cb07515a1e74797f878c0
Morgan <morgan.smith118@gmail.com>
2021-07-27 12:24:28 -0600

    user the short pwdstring duh

--- assets/js/p1Logon.js
+++ assets/js/p1Logon.js
@@ -647,7 +647,7 @@ function parsepwned(data,password){
   for (i=0; i<pwdArry.length; i++){
     let value = pwdArry[i];
     console.log("current value: " + value);
-    if(value.includes(password)){
+    if(value.includes(pwdstring)){
       console.log("BREACHED");
       pwned("BREACHED");
       break;
